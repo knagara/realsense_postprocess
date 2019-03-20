fc0dee9cec12613f979cdce8bf3e0dd6f282d664
knagara <nagara.keita@gmail.com>
2019-03-20 13:57:46 +0900

    change filter method in cloud_filter

--- src/cloud_filter.cpp
+++ src/cloud_filter.cpp
@@ -2,52 +2,55 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 
-#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
 
 #include <realsense_postprocess/cloud_operator.h>
-
 
 class CloudFilter : public CloudOperator
 {
 public:
-    CloudFilter(ros::NodeHandle &nh) :
-        cloud_pub_(nh.advertise<sensor_msgs::PointCloud2>("cloud_filtered", 1))
-    {}
+  CloudFilter(ros::NodeHandle &nh) : cloud_pub_(nh.advertise<sensor_msgs::PointCloud2>("cloud_filtered", 1))
+  {
+  }
 
-    void operate()
-    {
-        pcl::PointCloud<pcl::PointXYZ> cloud_input_pcl;
-        pcl::PointCloud<pcl::PointXYZ> cloud_filtered_pcl;
+  void operate()
+  {
+    pcl::PointCloud<pcl::PointXYZ> cloud_input_pcl;
+    pcl::PointCloud<pcl::PointXYZ> cloud_filtered_pcl;
 
-        pcl::fromROSMsg(cloud_input_ros_, cloud_input_pcl);
+    pcl::fromROSMsg(cloud_input_ros_, cloud_input_pcl);
 
-        pcl::StatisticalOutlierRemoval<pcl::PointXYZ> statFilter;
-        statFilter.setInputCloud(cloud_input_pcl.makeShared());
-        statFilter.setMeanK(10);
-        statFilter.setStddevMulThresh(0.2);
-        statFilter.filter(cloud_filtered_pcl);
+    applyDownSampler(cloud_input_pcl, &cloud_filtered_pcl);
 
-        pcl::toROSMsg(cloud_filtered_pcl, cloud_filterd_ros_);
-    }
+    pcl::toROSMsg(cloud_filtered_pcl, cloud_filterd_ros_);
+  }
 
-    void publish()
-    {
-        cloud_pub_.publish(cloud_filterd_ros_);
-    }
+  void publish()
+  {
+    cloud_pub_.publish(cloud_filterd_ros_);
+  }
+
+private:
+  void applyDownSampler(const pcl::PointCloud<pcl::PointXYZ> &input, pcl::PointCloud<pcl::PointXYZ> *output)
+  {
+    pcl::VoxelGrid<pcl::PointXYZ> voxelSampler;
+    voxelSampler.setInputCloud(input.makeShared());
+    voxelSampler.setLeafSize(0.1f, 0.1f, 0.1f);
+    voxelSampler.filter(*output);
+  }
 
 protected:
-    ros::Publisher cloud_pub_;
-    sensor_msgs::PointCloud2 cloud_filterd_ros_;
+  ros::Publisher cloud_pub_;
+  sensor_msgs::PointCloud2 cloud_filterd_ros_;
 };
-
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "realsense_postprocess_cloud_filter");
-    ros::NodeHandle nh;
+  ros::init(argc, argv, "realsense_postprocess_cloud_filter");
+  ros::NodeHandle nh;
 
-    CloudOperationHandler handler(nh, new CloudFilter(nh), "/camera/depth/color/points");
+  CloudOperationHandler handler(nh, new CloudFilter(nh), "/camera/depth/color/points");
 
-    ros::spin();
-    return 0;
+  ros::spin();
+  return 0;
 }
